c78fbd793aeb775912bf5a1856b2478d8b27a903
Patrick Cho <pcho3098@conestogac.on.ca>
2021-04-19 20:16:23 -0400

    Created classifier class, int data member, and setter/getter functions

---
+++ classifier.cpp
@@ -0,0 +1,20 @@
+/*Filename: classifier.cpp
+* Assignment: Assignment-5: Version Control
+ *By: Patrick Cho
+ *Date: April 19, 2021
+ *Description: This file contains a Classifier class, containing an int and two simple functions.  This
+ *			   is being used to practice with Git and Github.
+*/
+
+
+
+class Classifier {
+	int data;
+public:
+	void setData(int dataInput) {
+		data = dataInput;
+	}
+	int data() {
+		return data;
+	}
+};
